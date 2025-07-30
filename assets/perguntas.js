891e4109a6bb5bfda382199cb708dc87eb62428a
Braianvilabela42 <machado.braian@escola.pr.gov.br>
2025-07-30 12:52:36 +0000

    perguntas

---
+++ assets/perguntas.js
@@ -0,0 +1,15 @@
+  criarcartao(
+     "astrofisica",
+     "O que é o horizinte de eventos de um buraco negro ?",
+     " O limite além do qual nada pode escapar,nem mesmo a luz"
+  )
+   criacartao(
+     "astrofisica",
+      "Qual evidência obsrvacional apoia fortemente a existencia da energia escura ?",
+      "A aceleração da expanção do universo"
+   )
+   criacartao(
+    "astrofisica",
+    "Porque a radiação cosmica de fundo é uma evidência do big bang ?",
+    " Por que representa a radiação térmica renacente do universo primitivo "
+   )
