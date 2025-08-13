b1a34638027f19514e9919a3c6d703a9b85477a2
Braianvilabela42 <machado.braian@escola.pr.gov.br>
2025-08-13 13:49:38 +0000

    sla oq eu fiz aqui

--- assets/app.js
+++ assets/app.js
@@ -3,3 +3,25 @@ function criacartao(categoria, pergunta, resposta){
  const cartao = document.createElement('article')
 cartao.className = 'cartao'
 }
+
+cartao.innerHTML = `
+        <div class="cartao_conteudo">
+            <h3>${categoria}</h3>
+            <div class="pergunta">
+                ${pergunta}
+            </div>
+            <div class="resposta">
+                ${resposta}
+            </div>
+        </div>
+    `
+            let respotavisivel = false
+            function viraCartao(){
+                resposivel = respotavisivel
+                cartao.classlist.toggle("active", respotavisivel)
+            }
+            cartao.addvertListner()
+    cartao.innerHTML =
+    <div class="cartao_conteudo"
+
+        }
