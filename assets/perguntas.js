2962bc7b98b1237c5309680162ad704cc97d7ef7
Braianvilabela42 <machado.braian@escola.pr.gov.br>
2025-08-13 11:19:26 +0000

    comando de bloco

--- assets/perguntas.js
+++ assets/perguntas.js
@@ -13,3 +13,18 @@
     "Porque a radiação cosmica de fundo é uma evidência do big bang ?",
     " Por que representa a radiação térmica renacente do universo primitivo "
    )
+   criarcartao(
+      "astrofisica",
+      "O que é a matéria escura e como os astrônomos a detectam?",
+      "A matéria escura é uma substância misteriosa que compõe cerca de 27% do universo, mas não emite, absorve ou reflete luz, tornando-a invisível. Ela é detectada por seus efeitos gravitacionais sobre a matéria visível. Por exemplo, astrônomos observam como a matéria visível (como estrelas e galáxias) se move e percebe-se que há mais gravidade agindo sobre elas do que seria possível pela quantidade de matéria visível observada, sugerindo a presença de matéria escura."
+   )
+   criarcartao(
+      "astrofisica",
+      "como os buracos negros se formam e qual é a relação deles com a teoria da relatividade de Einstein?",
+      "Buracos negros se formam quando uma estrela massiva esgota seu combustível nuclear e colapsa sob sua própria gravidade, criando uma região de espaço-tempo onde a gravidade é tão intensa que nada, nem a luz, pode escapar. A teoria da relatividade geral de Einstein prevê que a gravidade pode curvar o espaço-tempo, e um buraco negro é uma região onde essa curvatura é tão extrema que forma uma "singularidade", um ponto de densidade infinita."
+   )
+   criarcartao(
+      "astrofisica",
+      "O que acontece quando uma estrela massiva explode em uma supernova?",
+      "Quando uma estrela massiva chega ao fim de sua vida, ela pode passar por uma explosão chamada supernova. Esse evento ocorre quando o núcleo da estrela colapsa devido à gravidade, liberando uma enorme quantidade de energia. A explosão de supernova pode resultar na formação de um buraco negro ou uma estrela de nêutrons, e a matéria da estrela é dispersada no espaço, enriquecendo o meio interestelar com elementos pesados, como ferro, carbono e oxigênio."
+   )
